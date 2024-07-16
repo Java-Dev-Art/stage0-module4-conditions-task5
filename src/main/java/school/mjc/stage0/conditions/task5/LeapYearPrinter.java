fbb4e5ca3753ed023d86c7638f6d3a1f349af268
Artur Markouski <markouski.artur@student.ehu.lt>
2024-07-16 15:29:34 +0200

    feat: add solv 4 tasks

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        String leapCheck = (year % 400 == 0)? "leap":
+                          (year % 100 == 0) ? "not leap" :
+                             (year % 4 == 0) ? "leap" : "not leap";
+        switch (leapCheck){
+            case "leap":
+                System.out.println("leap");
+                break;
+            default:
+                System.out.println("not leap");
+                break;
+        }
     }
 }
